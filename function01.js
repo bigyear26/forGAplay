0270f459176fb7676ce4d33bf1e9fe712574ccc9
harry <bigyear26@gmail.com>
2018-07-05 18:01:59 +0800

    虛擬網頁測試

--- function01.js
+++ function01.js
@@ -1,3 +1,39 @@
 function m01() {
     document.getElementById('m01').textContent='幹幹';
 }
+
+// var taiwan01={
+//     taiwan: [
+//         cool:'cool',
+//         {child:['yue','eee'],woman:['alice','may']},
+//         "基隆",
+//         "臺中",
+//         "臺南",
+//         "高雄",
+//         "臺北",
+//         "宜蘭",
+//         "桃園",
+//         "新竹",
+//         "苗栗",
+//         "彰化",
+//         "臺中",
+//         "南投",
+//         "臺南",
+//         "嘉義",
+//         "雲林",
+//         "高雄",
+//         "屏東",
+//         "臺東",
+//         "花蓮",
+//         "澎湖"
+//     ]
+// };
+//
+//
+// console.log(taiwan01.taiwan[1].woman[1]);
+
+
+
+var kids={'小明':["愛生氣","愛打鬧"],'小華':'好笑'};
+
+console.log(kids.小明[1]);
